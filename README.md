a94a75bcce60a85abf4802752dc95a78a5623364
kip693 <33307300+kip693@users.noreply.github.com>
2021-07-04 12:05:05 +0900

    fix:csvの出力を正常に

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 必要なnode_modulesをインストールします。
 puppeteerが含まれており、その中にchromiumが入っているので若干時間かかるかも。
 
+# 実行方法
+`node index.js`
+
+ルートディレクトリで実行すること。
--- index.js
+++ index.js
@@ -11,34 +11,47 @@ const path = require('path');
 
   try {
     await page.goto('https://visionfund.com/portfolio');
-    var description = [];
+    var data = [];
+
     for (let t = 1; t < 10; t++) {
       var section = '/html/body/main/div/div/div/div[3]/div/div[2]/div[' + t + ']/div[1]/table/tbody/tr['
 
       try {
         for (let i = 1; i <= 50; i++) {
+          let description = [];
           let xpath = section + i + ']/td[2]/div[2]/span'
           const elems = await page.$x(xpath);
           const jsHandle = await elems[0].getProperty('textContent');
           const text = await jsHandle.jsonValue();
           description.push(text);
-          console.log(text);
+          console.log(description)
+          data.push(description);
         }
 
       } catch (err) {
+        console.log(err)
       }
 
     }
   } catch (err) {
     // エラーが起きた際の処理
+    console.log(err)
   } finally {
-    stringify(description, (error, csvString) => {
+
+    
+
+    console.log(data)
+    console.log("success")
+    // CSVに出力する処理.
+    // data(多次元配列)
+    stringify(data, (error, csvString) => {
       // ファイルシステムに対してファイル名を指定し、ファイルストリームを生成する.
-      const writableStream = fs.createWriteStream(path.join(__dirname, 'visionfund.csv'));
+      const writableStream = fs.createWriteStream(path.join(__dirname, 'result.csv'));
       // csvStringをUTF-8で書き出す.
       writableStream.write(iconv.encode(csvString, 'UTF-8'));
     });
-    
+
+
     await browser.close();
   }
 })();
